5d2870881fe1e32c954d040c90691f8092b5247a
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-04 00:49:30 +0600

    user can delete mark todo as complete

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="todo-item" v-bind:class="{ 'is-completed': todo.completed }">
+		<p>
+			<input type="checkbox" v-on:change="markComplete" />
+			{{ todo.title }}
+		</p>
+		<button class="delete-item" @click="$emit('remove-todo', todo.id)">
+			X
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TodoItem',
+	props: ['todo'],
+	methods: {
+		markComplete() {
+			this.todo.completed = !this.todo.completed;
+		}
+	}
+};
+</script>
+
+<style scoped>
+.todo-item {
+	background: #e3e3e3;
+	font-size: 18px;
+	padding: 20px 10px;
+	border-bottom: 1px solid #eeeeee;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+}
+
+.is-completed {
+	text-decoration: line-through;
+}
+
+.delete-item {
+	background: rgb(153, 26, 26);
+	padding: 12px;
+	font-size: 18px;
+	border: none;
+	cursor: pointer;
+	color: #ffffff;
+	border-radius: 3px;
+}
+</style>
